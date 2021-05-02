afcef5a9566984f320f6fbcd037f4a7553fb4dc0
JungSeongyun <>
2021-05-02 15:36:02 +0000

    Made a new file named 'temp_functions' and copy and paste the 'fahr_to_celsius' function and ' temp_ classifier' function.

---
+++ temp_functions.py
@@ -0,0 +1,14 @@
+def fahr_to_celsius(temp_fahrenheit):
+  converted_tmp = (temp_fahrenheit - 32) / 1.8
+  return converted_tmp
+
+def temp_classifier(temp_celsius):
+  if temp_celsius < -2:#Check from the most strict value.
+    return 0
+  elif temp_celsius < 2:
+    return 1
+  elif temp_celsius < 15:
+    return 2
+  else:
+    return 3 
+    
